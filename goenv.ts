518af481bf2dd3dfd4b5767de20a06375b24b18c
iz <2131601562@qq.com>
2024-08-04 14:13:30 +0800

    permission

--- goenv.ts
+++ goenv.ts
@@ -3,34 +3,34 @@ export default {
         'GOOS': 'linux',
         'GOARCH': 'arm64',
         'GOARM': '8',
-        'CC': 'aarch64-linux-musl-gcc'
+        'CC': '~/toolchain/bin/aarch64-linux-musl-gcc'
     },
     'armv7l': {
         'GOOS': 'linux',
         'GOARCH': 'arm',
         'GOARM': '7',
-        'CC': 'armv7l-linux-musleabihf-gcc'
+        'CC': '~/toolchain/bin/armv7l-linux-musleabihf-gcc'
     },
     'armv5': {
         'GOOS': 'linux',
         'GOARCH': 'arm',
         'GOARM': '5',
-        'CC': 'arm-linux-musleabi-gcc'
+        'CC': '~/toolchain/bin/arm-linux-musleabi-gcc'
     },
     'x86_64': {
         'GOOS': 'linux',
         'GOARCH': 'amd64',
-        'CC': 'x86_64-linux-musl-gcc'
+        'CC': '~/toolchain/bin/x86_64-linux-musl-gcc'
     },
     'windows': {
         'GOOS': 'windows',
         'GOARCH': 'amd64',
-        'CC': 'x86_64-w64-mingw32-gcc'
+        'CC': '~/toolchain/bin/x86_64-w64-mingw32-gcc'
     },
     'armeb': {
         'GOOS': 'linux',
         'GOARCH': 'arm',
         'GOARM': '5',
-        'CC': 'aarch64-linux-musl-gcc'
+        'CC': '~/toolchain/bin/aarch64-linux-musl-gcc'
     }
 };
--- setup.ts
+++ setup.ts
@@ -2,34 +2,40 @@ import CC from './cc.ts';
 import GOEnv from './goenv.ts';
 import { run, wget } from './utils.ts';
 
-run('mkdir -p /dist/');
+run('mkdir -p ~/dist/');
+run('mkdir -p ~/root/');
+run('mkdir -p ~/toolchain/');
+
+// 获取前端文件包
+Deno.chdir('~/');
+await wget('https://github.com/imzlh/alist-ui-vlist/releases/download/master/vlist5_latest.tgz', 'vlist.tgz');
+run(`tar -xf vlist.tgz`);
+
+// 克隆alist仓库
+run(`git clone https://github.com/alist-org/alist.git`);
+run(`mv dist/ alist/public/`);
 
 for(const arch in CC){
     const cc = (CC as Record<string, string>)[arch],
         env = (GOEnv as Record<string, Record<string, string>>)[arch];
 
     // 清空home目录
-    run('rm -rf /home/*');
+    run('rm -rf ~/root/*');
 
     // 安装依赖
-    Deno.chdir('/usr/local/');
+    Deno.chdir('~/toolchain/');
 
     // 获取编辑工具包
-    wget(cc, arch + '.tgz');
+    run(`rm -rf *`)
+    await wget(cc, arch + '.tgz');
     run(`tar -xf ${arch}.tgz`);
     run(`cp -r *-cross/* ./`);
     run(`cp -r *-native/* ./`);
     run(`rm ${arch}.tgz`);
 
-    // 获取前端文件包
-    Deno.chdir('/home/');
-    wget('https://github.com/imzlh/alist-ui-vlist/releases/download/master/vlist5_latest.tgz', 'vlist.tgz');
-    run(`tar -xf vlist.tgz`);
-
-    // 克隆alist仓库
-    run(`git clone https://github.com/alist-org/alist.git`);
-    Deno.chdir('alist/');
-    run(`mv ../dist ./public/`);
+    // 复制
+    Deno.chdir('~/root/');
+    run(`cp -r ~/alist/ .`);
 
     // 开始编译
     const addition = `X 'github.com/alist-org/alist/v3/internal/conf.BuiltAt=${new Date().toISOString()}'
@@ -41,6 +47,6 @@ for(const arch in CC){
 -X 'github.com/alist-org/alist/v3/internal/conf.Version=${run("git describe --long --tags --dirty --always")}' 
 -X 'github.com/alist-org/alist/v3/internal/conf.WebVersion=5.6'`;
     run(`go build --ldflags="-s -w ${addition}" -o alist`, env);
-    run(`mv alist /dist/alist-${arch}`);
+    run(`mv alist ~/dist/alist-${arch}`);
     run(`rm -rf alist`);
 }
